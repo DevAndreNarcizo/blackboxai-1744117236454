707aee50c015bda17ca3ffb46f00387b95225cda
André Narcizo <dev.andrenarcizo@gmail.com>
2025-04-08 10:00:46 -0300

    Add src/main.py 1744117244434

---
+++ src/main.py
@@ -0,0 +1,7 @@
+# Este é o ponto de entrada para o aplicativo de gestão financeira pessoal.
+
+def main():
+    print("Bem-vindo ao aplicativo de gestão financeira pessoal!")
+
+if __name__ == "__main__":
+    main()
